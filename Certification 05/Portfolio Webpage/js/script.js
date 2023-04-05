d25ae081b8a2bebff5c594db84b498f8a6ac520d
anuhas-akalanka <akalanka.anuhas@gmail.com>
2023-04-05 14:21:28 +0530

    Portfolio website done.

---
+++ Certification 05/Portfolio Webpage/js/script.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+// Close navbar when link is clicked
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
